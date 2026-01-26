fb2a3977f4c66fc1b689eaf258bbb41e7016b69e
GErP83 <gurrka@gmail.com>
2026-01-26 16:29:35 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -28,6 +28,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.6.0"),
+        // [docc-plugin-placeholder]
     ],
     
     targets: [
